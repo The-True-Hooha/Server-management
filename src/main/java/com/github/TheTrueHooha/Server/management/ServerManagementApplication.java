2d8d29c30cefdff6e4b0d3601eb2147149d5dbf7
David Ogar <owogogahhero@outlook.com>
2022-04-09 10:37:08 +0100

    adding files to remote

---
+++ src/main/java/com/github/TheTrueHooha/Server/management/ServerManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.TheTrueHooha.Server.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/TheTrueHooha/Server/management/ServerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.TheTrueHooha.Server.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
