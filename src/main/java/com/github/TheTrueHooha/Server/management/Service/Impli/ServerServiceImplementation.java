a7bab1e4cc8187e9797b0f57a2b91c391be34c5f
David Ogar <owogogahhero@outlook.com>
2022-04-10 06:25:09 +0100

    added ping method to repo

--- src/main/java/com/github/TheTrueHooha/Server/management/Service/Impli/ServerServiceImplementation.java
+++ src/main/java/com/github/TheTrueHooha/Server/management/Service/Impli/ServerServiceImplementation.java
@@ -1,15 +1,44 @@
 package com.github.TheTrueHooha.Server.management.Service.Impli;
 
+import com.github.TheTrueHooha.Server.management.Enums.ServerStatus;
 import com.github.TheTrueHooha.Server.management.Model.Servers;
+import com.github.TheTrueHooha.Server.management.Repository.ServerRepository;
 import com.github.TheTrueHooha.Server.management.Service.ServerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 
 public class ServerServiceImplementation implements ServerService {
 
+    private final ServerRepository serverRepository;
+
+    //creates a new server and saves to the repo
     @Override
     public Servers createServer(Servers servers) {
-        return null;
+        log.info("saving your new server: {}", servers.getServerName());
+        servers.setImageUrl(setServerImageUrl());
+        return serverRepository.save(servers);
+    }
+
+    //method pings the server and return a timeout to show active status
+    @Override
+    public Servers pingServer(String ipAddress) throws IOException {
+        log.info("pinging...IP: {}", ipAddress);
+        Servers servers = serverRepository.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        servers.setServerStatus(address.isReachable(10000) ? ServerStatus.SERVER_RUNNING : ServerStatus.SERVER_DOWN);
+        serverRepository.save(servers);
+        return servers;
     }
 
     @Override
@@ -31,4 +60,9 @@ public class ServerServiceImplementation implements ServerService {
     public Boolean deleteServer(Long id) {
         return null;
     }
+
+    private String setServerImageUrl() {
+        return null;
+    }
+
 }
--- src/main/java/com/github/TheTrueHooha/Server/management/Service/ServerService.java
+++ src/main/java/com/github/TheTrueHooha/Server/management/Service/ServerService.java
@@ -2,12 +2,16 @@ package com.github.TheTrueHooha.Server.management.Service;
 
 import com.github.TheTrueHooha.Server.management.Model.Servers;
 
+import java.io.IOException;
 import java.util.Collection;
 
 public interface ServerService {
 
     //creates a new server and saves to the database
     Servers createServer (Servers servers);
+
+    //to ping the server if it's active or not
+    Servers pingServer (String ipAddress) throws IOException;
 
     //list the servers created
     Collection<Servers> listServers(int limit);
